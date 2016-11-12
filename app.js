8bafb944282cd163c132ddf7be139ed860970d10
Daniel Giles <d_giles123@hotmail.com>
2016-11-12 10:00:13 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+//modal views
+function showguitaristmodal() {
+  $('#guitaristmodal')
+    .modal('show');
+
+}
+
+function showdrummermodal() {
+  $('#drummermodal')
+    .modal('show');
+
+}
+
+function showvocalistmodal() {
+  $('#vocalistmodal')
+    .modal('show');
+
+}
