c8c98016003d023fdab7d1d58b7e1f2039556a24
DESKTOP-OT1NJ8J\d_gil <d_giles123@hotmail.com>
2017-07-09 15:27:36 -0400

    Moving old stuff into a history/archive

---
+++ old/app.js
@@ -0,0 +1,18 @@
+
+//modal views
+function guitaristmodal(action) {
+  $('#guitaristmodal')
+    .modal(action);
+
+}
+
+function drummermodal(action) {
+  $('#drummermodal')
+    .modal(action);
+
+}
+
+function vocalistmodal(action) {
+  $('#vocalistmodal')
+    .modal(action);
+}
