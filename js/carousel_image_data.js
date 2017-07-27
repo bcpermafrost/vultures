7c762dc1316f8b0b16125ddc7d4cf2520ac2d8cf
Daniel Giles <d_giles123@hotmail.com>
2017-07-26 23:45:29 -0400

    Progress on Carousel.... Updated CSS

---
+++ js/carousel_image_data.js
@@ -0,0 +1,37 @@
+var data =  [
+    {
+      image: "/images/band1-min.jpg",
+      text: "band",
+      id: 0
+    },
+    {
+      image: "/images/darren1-min.jpg",
+      text: "darren1",
+      id: 1
+    },
+    {
+      image: "/images/corey1-min.jpg",
+      text: "corey1",
+      id: 2
+    },
+    {
+      image: "/images/darren2-min.jpg",
+      text: "darren2",
+      id: 3
+    },
+    {
+      image: "/images/corey2-min.jpg",
+      text: "corey2",
+      id: 4
+    },
+    {
+      image: "/images/alan2-min.jpg",
+      text: "corey1",
+      id: 5
+    },
+    {
+      image: "/images/alan1-min.jpg",
+      text: "corey1",
+      id: 6
+    }
+  ];
