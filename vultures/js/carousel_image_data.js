47a0e1a66cf07538c859397c5487c028cd347952
DESKTOP-S55P2U5\d_gil <d_giles123@hotmail.com>
2017-09-02 08:33:40 -0400

    moved into different structure

--- vultures/js/carousel_image_data.js
+++ vultures/js/carousel_image_data.js
@@ -1,36 +1,36 @@
 var data =  [
     {
-      image: "/images/band1-min.jpg",
+      image: "/vultures/images/band1-min.jpg",
       text: "band",
       id: 0
     },
     {
-      image: "/images/darren1-min.jpg",
+      image: "/vultures/images/darren1-min.jpg",
       text: "darren1",
       id: 1
     },
     {
-      image: "/images/corey1-min.jpg",
+      image: "/vultures/images/corey1-min.jpg",
       text: "corey1",
       id: 2
     },
     {
-      image: "/images/darren2-min.jpg",
+      image: "/vultures/images/darren2-min.jpg",
       text: "darren2",
       id: 3
     },
     {
-      image: "/images/corey2-min.jpg",
+      image: "/vultures/images/corey2-min.jpg",
       text: "corey2",
       id: 4
     },
     {
-      image: "/images/alan2-min.jpg",
+      image: "/vultures/images/alan2-min.jpg",
       text: "corey1",
       id: 5
     },
     {
-      image: "/images/alan1-min.jpg",
+      image: "/vultures/images/alan1-min.jpg",
       text: "corey1",
       id: 6
     }
